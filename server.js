a8b04c577890db9248200135c9d055ba5b8c8983
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-03-19 22:17:33 -0700

    Added middleware, change the name of Develop folder to assets and import the note database

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const note_db = require('./assets/db/db.json');
+
+
+// Use middleware to parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 
