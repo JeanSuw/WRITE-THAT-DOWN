fc0c64e081ae39485b9f92d19ebea20b228eb001
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-03-20 21:52:25 -0700

    Delete line 23 to 25

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
+
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
